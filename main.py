c0f0b265e73358883447a98711f457f032aa33f2
Geonhee123 <leeshak10218@gmail.com>
2025-06-09 23:43:05 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
@@ -17,5 +17,6 @@ st.write("시의 주제 : " + subject)
 
 
 if st.button("시 작성"):
-    result = chat_model.invoke(subject + "에 대한 시를 써줘")
-    st.write(result.content)
+    with st.spinner("시 작성중 ..."):
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
+        st.write(result.content)
